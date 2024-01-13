8f8bd2cc46790b7d59ab74a2ff9a417a783ed258
Ashikur Rahman <ashikur.jebu111@gmail.com>
2024-01-13 18:19:19 +0600

    bulding admin dashboard

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -3,7 +3,19 @@ import React from "react";
 const AdminDashboard = () => {
     return (
         <div className="relative w-full h-full">
-            <div className="w-full h-full ">AdminDashboard</div>
+            <h1 className="text-lg font-semibold mb-5">Dashboard</h1>
+
+            <div className="grid grid-cols-3 gap-4">
+                <div className="w-full h-20 p-2 rounded-sm shadow-sm border">
+                    AdminDashboard
+                </div>
+                <div className="w-full h-20 p-2 rounded-sm shadow-sm border">
+                    AdminDashboard
+                </div>
+                <div className="w-full h-20 p-2 rounded-sm shadow-sm border">
+                    AdminDashboard
+                </div>
+            </div>
         </div>
     );
 };
